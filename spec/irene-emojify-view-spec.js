ac654fc50d764c58b5bb6e4cc956658dd2ad1bc1
Irene Chen <chen.irene@gmail.com>
2018-02-05 16:05:07 -0800

    initial commit

---
+++ spec/irene-emojify-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IreneEmojifyView from '../lib/irene-emojify-view';
+
+describe('IreneEmojifyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
